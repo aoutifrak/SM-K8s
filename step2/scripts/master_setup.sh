507ca355e9897be6bcd24d0b7e730c660c7fd24f
kali <amine201017@hotmail.fr>
2025-02-25 13:37:38 +0000

    create replicas for app2 and mapp all the apps to hosts machine

--- step2/scripts/master_setup.sh
+++ step2/scripts/master_setup.sh
@@ -3,10 +3,13 @@ export INSTALL_K3S_EXEC="server \
   --node-ip=$1 \
   --advertise-address=$1 \
   --bind-address=$1 \
-  --write-kubeconfig-mode 644"
+  --write-kubeconfig-mode 644
+  "
 
 curl -sfL https://get.k3s.io | sh
 # echo 'alias k="/usr/local/bin/k3s"' > ~/.bashrc this is not working
 ln -s /usr/local/bin/k3s /bin/k
+
+echo "192.168.56.10 app1.com app2.com" | sudo tee -a /etc/hosts
 
 k kubectl apply -f /vag/k3s-deploy/app1/
