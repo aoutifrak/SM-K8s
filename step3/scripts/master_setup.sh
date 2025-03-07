ea0f08c2e2011faed3d2e5f4f7939167463c4872
kali <amine201017@hotmail.fr>
2025-03-07 23:32:13 +0000

    k3d installation along with dokcer

--- step3/scripts/master_setup.sh
+++ step3/scripts/master_setup.sh
@@ -1,17 +1,24 @@
+# install Docker [#ref https://docs.docker.com/engine/install/ubuntu/]
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-export INSTALL_K3S_EXEC="server \
-  --node-ip=$1 \
-  --advertise-address=$1 \
-  --bind-address=$1 \
-  --write-kubeconfig-mode 644
-  "
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable --now docker
+newgrp docker
 
-curl -sfL https://get.k3s.io | sh
-# echo 'alias k="/usr/local/bin/k3s"' > ~/.bashrc this is not working
-ln -s /usr/local/bin/k3s /bin/k
-
-kubectl create namespace argocd
-
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-kubectl apply -f /vag/k3s-deploy/apps/
+#install kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl 
+sudo mv kubectl /bin/
+#install K3d 
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
